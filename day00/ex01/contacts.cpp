8c6b18f0686e8f035c9df48cf801dcf72961e19a
yassine-alaoui <alaoui6b@gmail.com>
2021-06-03 20:31:38 +0100

    final stuff

--- day00/ex01/contacts.cpp
+++ day00/ex01/contacts.cpp
@@ -1,92 +1,155 @@
 #include "lib.hpp"
 
-int main()
+# define cin std::cin
+# define cout std::cout
+# define cerr std::cerr
+# define endl std::endl
+# define string std::string
+
+void	creat_contact(Contact *NewContact)
 {
-	std::string command;
-	int			i;
-	int			j;
-	int			thewho;
-	bool		done;
-	Contact contact[8];
+	string	tempo;
+
+	cout << "Creating new contact please write the \033[4;3;102;30mfirst name\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_FirstName(tempo);
+	cout << "Please write the \033[4;3;102;30mlast name\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_LastName(tempo);
+	cout << "Please write the \033[4;3;102;30mnick name\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_NickName(tempo);
+	cout << "Please write the \033[4;3;102;30mlogin\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_Login(tempo);
+	cout << "Please write the \033[4;3;102;30mpostal address\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_Postal_Address(tempo);
+	cout << "Please write the \033[4;3;102;30memail address\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_Email_Address(tempo);
+	cout << "Please write the \033[4;3;102;30mphone number\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_PhoneNum(tempo);
+	cout << "Please write the \033[4;3;102;30mBirthDate\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_BirthDate(tempo);
+	cout << "Please write the \033[4;3;102;30mFavorite meal\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_Favorite_Meal(tempo);
+	cout << "Please write the \033[4;3;102;30mUnderear color\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_Underwear_Color(tempo);
+	cout << "Please write the \033[4;3;102;30mDarkest Secret\033[0m of the new contact :" << endl << "=>>";
+	std::getline(cin, tempo);
+	(*NewContact).set_Darkest_Secret(tempo);
+}
+
+void	print_contact(Contact NewContact)
+{
+	cout << "FirstName: -->>";
+	cout << NewContact.get_FirstName() << endl;
+	cout << "LastName: -->>";
+	cout << NewContact.get_LastName() << endl;
+	cout << "NickName: -->>";
+	cout << NewContact.get_NickName() << endl;
+	cout << "PhoneNumber: -->>";
+	cout << NewContact.get_PhoneNum() << endl;
+	cout << "Login: -->>";
+	cout << NewContact.get_Login() << endl;
+	cout << "PostalAddress: -->>";
+	cout << NewContact.get_Postal_Address() << endl;
+	cout << "EmailAddress: -->>";
+	cout << NewContact.get_Email_Address() << endl;
+	cout << "Birthday: -->>";
+	cout << NewContact.get_BirthDate() << endl;
+	cout << "FavoriteMeal: -->>";
+	cout << NewContact.get_Favorite_Meal() << endl;
+	cout << "UnderwearColor: -->>";
+	cout << NewContact.get_Underwear_Color() << endl;
+	cout << "DarkestSecret: -->>";
+	cout << NewContact.get_Darkest_Secret() << endl;
+}
+
+void	search_contact(Contact *NewContact, int N)
+{
+	int 	i;
+	string	tmp;
 
 	i = 0;
-	std::cout << "\033[33;21mUse ADD to add new contact, SEARCH to look for a contact and EXIT to quit.\033[0m" << std::endl;
-	while (i < 8)
+	while (i < N)
 	{
-		command = "";
-		std::cout << "\033[33mWaiting for command :\033[0m" << std::endl;
-		std::cin >> command;
-		contact[i].iscreated = 0;
+		cout << std::setw(11);
+		cout << i << "|";
+		tmp = NewContact[i].get_FirstName();
+		tmp = tmp.substr(0,10);
+		if (tmp.length() > 9)
+			tmp = tmp.insert(10, ".");
+		else
+			cout << std::setw(11);
+		cout << tmp << "|";
+		tmp = NewContact[i].get_LastName();
+		tmp = tmp.substr(0,10);
+		if (tmp.length() > 9)
+			tmp = tmp.insert(10, ".");
+		else
+			cout << std::setw(11);
+		cout << tmp << "|";
+		tmp = NewContact[i].get_NickName();
+		tmp = tmp.substr(0,10);
+		if (tmp.length() > 9)
+			tmp = tmp.insert(10, ".");
+		else
+			cout << std::setw(11);
+		cout << tmp << "|" << endl;
+		i++;
+	}
+	cout << "\033[34;4mWhich index do you desire:\033[0m" << endl;
+	std::getline(cin, tmp);
+	if (tmp.length() == 1 && (tmp[0]) && std::stoi(tmp) < N)
+		print_contact(NewContact[std::stoi(tmp)]);
+	else
+		cerr << "\033[31mBad request\033[0m" << endl;
+}
+
+int	main()
+{
+	Contact	contact[8];
+	string	command;
+	int		num_cont;
+
+	num_cont = 0;
+	cout << "\033[33;21mUse ADD to add new contact, SEARCH to look for a contact and EXIT to quit.\033[0m" << endl;
+	while (1)
+	{
+		cout << "\033[33mWaiting for command :\033[0m" << endl;
+		std::getline(cin, command);
 		if (command == "EXIT")
 			return (0);
-		if (command == "SEARCH")
+		else if (command == "SEARCH")
 		{
-			j = 0;
-			done = 0;
-			if (i == 0)
-				std::cout << "Nothing found" << std::endl;
-			while (j < i)
+			if (num_cont == 0)
 			{
-				if (contact[j].iscreated == 1)
-				{
-					std::string firstname;
-					std::string lastname;
-					std::string nickname;
-
-					firstname = contact[j].FirstName.substr(0,10);
-					lastname = contact[j].LastName.substr(0,10);
-					nickname = contact[j].NickName.substr(0,10);
-					if (firstname.length() > 9)
-						firstname = firstname.insert(10, ".");
-					if (lastname.length() > 9)
-						lastname = lastname.insert(10, ".");
-					if (nickname.length() > 9)
-						nickname = nickname.insert(10, ".");
-					done = 1;
-					std::cout << j << "|" << firstname << "|" << lastname << "|" << nickname << std::endl;
-				}
-				if (done == 0)
-				{
-					std::cout << "Nothing found" << std::endl;
-					break ;
-				}
-				j++;
+				cerr << "No contacts found" << endl;
+				continue ;
 			}
-			if (done == 1)
-			{
-				std::cout << "\033[34;4mWhich index do you desire:\033[0m" << std::endl;
-				std::cin >> thewho;
-				std::cout << "\n" << contact[thewho].FirstName << "\n" << contact[thewho].LastName << "\n" << contact[thewho].NickName << "\n" << contact[thewho].Login << "\n"
-					<< contact[thewho].Postal_Address << "\n" << contact[thewho].Email_Address << "\n" << contact[thewho].BirthDate << "\n" << contact[thewho].Favorite_Meal << "\n"
-						<< contact[thewho].Underwear_Color << "\n" << contact[thewho].Darkest_Secret << std::endl;
-			}
+			search_contact(contact, num_cont);
 		}
-		if (command == "ADD")
+		else if (command == "ADD")
 		{
-			contact[i].iscreated = 1;
-			std::cout << "Creating new contact please write the \033[4;3;102;30mfirst name\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].FirstName;
-			std::cout << "Please write the \033[4;3;102;30mlast name\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].LastName;
-			std::cout << "Please write the \033[4;3;102;30mnick name\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].NickName;
-			std::cout << "Please write the \033[4;3;102;30mlogin\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].Login;
-			std::cout << "Please write the \033[4;3;102;30mpostal address\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].Postal_Address;
-			std::cout << "Please write the \033[4;3;102;30memail address\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].Email_Address;
-			std::cout << "Please write the \033[4;3;102;30mphone number\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].PhoneNum;
-			std::cout << "Please write the \033[4;3;102;30mBirthDate\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].BirthDate;
-			std::cout << "Please write the \033[4;3;102;30mFavorite meal\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].Favorite_Meal;
-			std::cout << "Please write the \033[4;3;102;30mUnderear color\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].Underwear_Color;
-			std::cout << "Please write the \033[4;3;102;30mDarkest Secret\033[0m of the new contact :" << std::endl << "=>>";
-			std::cin >> contact[i].Darkest_Secret;
-			i++;
+			if (num_cont >= 8)
+			{
+				cerr << "Enough for today!!" << endl;
+				continue ;
+			}
+			creat_contact(&contact[num_cont]);
+			num_cont++;
 		}
+		else
+			cerr << "\033[31mBad request\033[0m" << endl;
 	}
+	return (0);
 }
+
+
+
--- day00/ex01/lib.hpp
+++ day00/ex01/lib.hpp
@@ -2,21 +2,48 @@
 # define LIB_HPP
 #include <string>
 #include <iostream>
+#include <iomanip>
 
+# define string std::string
 class Contact {
-  public:
-	bool iscreated;
-    std::string PhoneNum;
-    std::string FirstName;
-    std::string LastName;
-    std::string NickName;
-    std::string Login;
-    std::string Postal_Address;
-    std::string Email_Address;
-    std::string BirthDate;
-    std::string Favorite_Meal;
-    std::string Underwear_Color;
-    std::string Darkest_Secret;
+	private:
+		string	PhoneNum;
+		string	FirstName;
+		string	LastName;
+		string	NickName;
+		string	Login;
+		string	Postal_Address;
+		string	Email_Address;
+		string	BirthDate;
+		string	Favorite_Meal;
+		string	Underwear_Color;
+		string	Darkest_Secret;
+	public:
+		// Setters
+		void set_PhoneNum(string value) { PhoneNum = value; };
+		void set_FirstName(string value) { FirstName = value; };
+		void set_LastName(string value) { LastName = value; };
+		void set_NickName(string value) { NickName = value; };
+		void set_Login(string value) { Login = value; };
+		void set_Postal_Address(string value) { Postal_Address = value; };
+		void set_Email_Address(string value) { Email_Address = value; };
+		void set_BirthDate(string value) { BirthDate = value; };
+		void set_Favorite_Meal(string value) { Favorite_Meal = value; };
+		void set_Underwear_Color(string value) { Underwear_Color = value; };
+		void set_Darkest_Secret(string value) { Darkest_Secret = value; };
+
+		// Getters
+		string	get_PhoneNum() { return (PhoneNum); };
+		string	get_FirstName() { return (FirstName); };
+		string	get_LastName() { return (LastName); };
+		string	get_NickName() { return (NickName); };
+		string	get_Login() { return (Login); };
+		string	get_Postal_Address() { return (Postal_Address); };
+		string	get_Email_Address() { return (Email_Address); };
+		string	get_BirthDate() { return (BirthDate); };
+		string	get_Favorite_Meal() { return (Favorite_Meal); };
+		string	get_Underwear_Color() { return (Underwear_Color); };
+		string	get_Darkest_Secret() { return (Darkest_Secret); };
 };
 
 #endif
